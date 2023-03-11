f2979444d027515dfc346f1fefcf55b416c40044
MohGinanjar <ginanjar.farin@gmail.com>
2023-03-11 07:37:39 +0700

    for static

--- vehicle/vehicle/settings.py
+++ vehicle/vehicle/settings.py
@@ -151,7 +151,15 @@ CORS_ORIGIN_REGEX_WHITELIST = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+if DEBUG:
+    STATICFILES_DIRS= [os.path.join(BASE_DIR, 'static')]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
